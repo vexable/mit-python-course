89756a2479ffe5a957837613ab6279dd38990756
Pranesh Sathish Kumar <praneshsathishk@gmail.com>
2022-09-10 16:47:13 -0400

    unit 2 guess your number assignment

--- Unit 2.py
+++ Unit 2.py
@@ -38,19 +38,18 @@
 high = 100
 low = 0
 print("Please think of a number between 0 and 100")
-guess = int((high+low)/2)
+guess = (high+low)//2
 while True:
-    print("Is your secret number " + str(int(guess)) + "?")
+    print("Is your secret number " + str(guess) + "?")
     response = str(input( "Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly "))
     if response.lower() == "h":
         high = guess
-        guess = (high+low)/2
+        guess = (high+low)//2
     elif response.lower() == "l":
         low = guess
-        guess = (high+low)/2
+        guess = (high+low)//2
     elif response.lower() == "c":
-        print("Game over. Your secret number was: " + str(int(guess)))
+        print("Game over. Your secret number was: " + str(guess))
         break
     else:
         print("Sorry, I did not understand your input.")
-print("Git commit test")
