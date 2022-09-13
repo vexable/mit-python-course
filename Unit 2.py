a36408a97c555d14e85d8668a45493faa8a3cacd
Pranesh Sathish Kumar <praneshsathishk@gmail.com>
2022-09-13 06:14:05 -0400

    Unit Commit

--- Unit 2.py
+++ Unit 2.py
@@ -47,13 +47,60 @@
 #print(result)
 
 #newton-raphson algorithm
-epsilon = 0.01 
-y = 54.0
-guess = y/2.0
-numGuesses = 0
-while abs(guess*guess) >= epsilon:
-    numGuesses += 1
-    guess = guess - (((guess**2)-y)/(2*guess))
-print(numGuesses)
-print(y)
-print(guess)
+# epsilon = 0.01 
+# y = 54.0
+# guess = y/2.0
+# numGuesses = 0
+# while abs(guess*guess) >= epsilon:
+#     numGuesses += 1
+#     guess = guess - (((guess**2)-y)/(2*guess))
+# print(numGuesses)
+# print(y)
+# print(guess)
+
+#function example
+# def is_even(i):
+#     """
+#     Input: i, a positive int
+#     Returns True if even, otherwise False
+#     """
+#     print("Hi")
+#     return i%2 == 0
+
+#function with variable scope
+# def f (x):
+#     x = x+1
+#     print("in f(x), x = ", x)
+#     return x
+# x=3
+# z= f(x)
+#result is 4
+
+#example of function as parameter/arguement
+# def func_a():
+#     print("inside func_a")
+#     return
+# def func_b(y):
+#     print("inside func_b")
+#     return y
+# def func_c(z):
+#     print("inside func_c")
+#     return z
+# print(func_a)
+# print(5 + func_b(2))
+# print(func_c(func_a))
+
+#name simple function
+# def printName(firstName,lastName,reverse):
+#     if reverse:
+#         print(lastName,firstName)
+#     else:
+#         print(firstName,lastName)
+# printName("Pranesh","Sathish Kumar","False")
+
+# def foo(x, y = 5):
+#    def bar(x):
+#       return x + 1
+#    return bar(y * 2)
+          
+# foo(3)
